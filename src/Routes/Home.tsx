bf1748b3230cfa08d4b4c1089b3301a68b493796
윤수영 <yunsuyeong@yunsuyeong-ui-MacBookAir.local>
2023-01-25 16:49:52 +0900

    2023/01/25(Create Slider)

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -43,14 +43,17 @@ const Slider = styled.div`
 
 const Row = styled(motion.div)`
   display: grid;
-  gap: 10px;
+  gap: 5px;
   grid-template-columns: repeat(6, 1fr);
   position: absolute;
   width: 100%;
 `;
 
-const Box = styled(motion.div)`
+const Box = styled(motion.div)<{ bgPhoto: string }>`
   background-color: white;
+  background-image: url(${(props) => props.bgPhoto});
+  background-size: cover;
+  background-position: center center;
   height: 200px;
   color: red;
   font-size: 66px;
@@ -58,15 +61,17 @@ const Box = styled(motion.div)`
 
 const rowVariants = {
   hidden: {
-    x: window.outerWidth + 10,
+    x: window.outerWidth + 5,
   },
   visible: {
     x: 0,
   },
   exit: {
-    x: -window.outerWidth - 10,
+    x: -window.outerWidth - 5,
   },
 };
+
+const number = 6;
 
 function Home() {
   const { isLoading, data } = useQuery<IGetMoviesResult>(
@@ -74,9 +79,17 @@ function Home() {
     getMovies
   );
   const [index, setIndex] = useState(0);
+  const [leaving, setLeaving] = useState(false);
   const increaseIndex = () => {
-    setIndex((prev) => prev + 1);
+    if (data) {
+      if (leaving) return;
+      toggleLeaving();
+      const total = data.results.length - 1;
+      const maxIndex = Math.floor(total / number) - 1;
+      setIndex((prev) => (prev === maxIndex ? 0 : prev + 1));
+    }
   };
+  const toggleLeaving = () => setLeaving((prev) => !prev);
   return (
     <Wrapper>
       {isLoading ? (
@@ -91,7 +104,7 @@ function Home() {
             <Overview>{data?.results[0].overview}</Overview>
           </Banner>
           <Slider>
-            <AnimatePresence>
+            <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
               <Row
                 variants={rowVariants}
                 initial="hidden"
@@ -100,9 +113,15 @@ function Home() {
                 transition={{ type: "tween", duration: 1 }}
                 key={index}
               >
-                {[1, 2, 3, 4, 5, 6].map((i) => (
-                  <Box key={i}>{i}</Box>
-                ))}
+                {data?.results
+                  .slice(1)
+                  .slice(number * index, number * index + number)
+                  .map((movie) => (
+                    <Box
+                      bgPhoto={makeImagePath(movie.backdrop_path, "w500")}
+                      key={movie.id}
+                    ></Box>
+                  ))}
               </Row>
             </AnimatePresence>
           </Slider>
